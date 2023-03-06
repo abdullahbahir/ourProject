dd2ab6114dbdfe5f3e3536dbd047b6d0ca34b2e3
Shukria.I <iqbal_shukria@hotmail.com>
2023-03-06 16:04:07 -0500

    my second clone

--- src/firstPackage/First.java
+++ src/firstPackage/First.java
@@ -15,8 +15,20 @@ public class First {
 	System.out.println(str);
 		
 		
-	
+	String str2= "our first clone in the gitHub repository"	;
+	System.out.println(str);
 		
+		
+	String str3= "our first clone in the gitHub repository"	;
+	System.out.println(str);
+	String str4= "our first clone in the gitHub repository"	;
+	System.out.println(str);
+	String st54= "our first clone in the gitHub repository"	;
+	System.out.println(str);
+	String str6= "our first clone in the gitHub repository"	;
+	System.out.println(str);
+	String str7= "our first clone in the gitHub repository"	;
+	System.out.println(str);
 		
 		
 		
