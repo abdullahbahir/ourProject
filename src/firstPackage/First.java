97ce49b1559c5b2c92f8b176614524af778b47c1
Abdullah Bahir <125522215+abdullahbahir@users.noreply.github.com>
2023-03-06 15:56:19 -0600

    Update First.java

--- src/firstPackage/First.java
+++ src/firstPackage/First.java
@@ -24,14 +24,7 @@ public class First {
 	
 	String str3= "our first clone in the gitHub repository"	;
 	System.out.println(str);
-	String str4= "our first clone in the gitHub repository"	;
-	System.out.println(str);
-	String st54= "our first clone in the gitHub repository"	;
-	System.out.println(str);
-	String str6= "our first clone in the gitHub repository"	;
-	System.out.println(str);
-	String str7= "our first clone in the gitHub repository"	;
-	System.out.println(str);
+
 		
 		
 	
